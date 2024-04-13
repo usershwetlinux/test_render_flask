467e8863937594266a147bbdb90e8695fa408cae
test <test@gmail.com>
2024-04-13 15:02:43 +0530

    intial_commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the homepage!'
+
+@app.route('/get_image')
+def get_image():
+    return send_from_directory('static', 'sample_image.jpg')
+
+if __name__ == '__main__':
+    app.run(debug=True)

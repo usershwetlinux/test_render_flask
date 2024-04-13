32664395c1bec1ff4aabc30575cf49a336c74075
test <test@gmail.com>
2024-04-13 15:16:04 +0530

    added upload fixed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/get_image')
 def get_image():
-    return send_from_directory('static', 'uploads/sample_image.jpg')
+    return send_from_directory('static/upload', 'example.jpg')
 
 if __name__ == '__main__':
     app.run(debug=True)
